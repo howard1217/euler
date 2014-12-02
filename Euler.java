77adfd635065c07ee75395a05120cd0551872375
Howard <howardchiao1217@gmail.com>
2014-12-02 14:07:26 +0100

    First Commit

---
+++ Euler.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+
+public class Euler {
+    public static void main(String[] args) {
+
+    }
+
+    /* Problem 9: Special Pythagorean triplet */
+    public static int SPT() {
+        
+    }
+}
