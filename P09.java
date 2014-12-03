7b28795d8c759fe6dcebaaf3b60d89440e241d72
Howard <howardchiao1217@gmail.com>
2014-12-03 23:20:09 +0100

    Changed file name

---
+++ P09.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.math.*;
+
+public class P09 {
+    public static void main(String[] args) {
+        long value, start, finish;
+        start = System.nanoTime();
+        int[] p9 = spt();
+        System.out.println("/* Problem 9 */");
+        System.out.printf("Answer: %s\n", p9[0] * p9[1] * p9[2]);
+        finish = System.nanoTime();
+        System.out.printf("Process Time = %s ms\n", (finish - start) / 1000000);
+    }
+
+    /* Problem 9: Special Pythagorean triplet */
+    public static int[] spt() {
+        int[] result = new int[3];
+        for (int a = 1; a < 1000; a += 1) {
+            for (int b = 1; b < 1000; b += 1) {
+                double c = Math.sqrt(a * a + b * b);
+                if (Math.floor(c) == c && (int) a + b + c == 1000) {
+                    result[0] = a;
+                    result[1] = b;
+                    result[2] = (int) c;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
